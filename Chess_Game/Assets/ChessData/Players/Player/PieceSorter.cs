a67403e51ebf547fae970a2939b682cecfe55422
kasi-mi <basisriceforty@yahoo.co.jp>
2023-04-26 10:42:54 +0900

    Add:PieceSorterクラスの追加

---
+++ Chess_Game/Assets/ChessData/Players/Player/PieceSorter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chess_Game.Chessset.Boards;
+using Chess_Game.Chessset.Pieces;
+
+namespace Chess_Game.Players.Player
+{
+    /// <summary>
+    /// 駒を盤上の正しい位置に配置するクラス
+    /// </summary>
+    public class PieceSorter
+    {
+
+        /// <summary>
+        /// 上側へ並べる
+        /// </summary>
+        public PieceList TopSideSort(Board board,PieceList pieceList)
+        {
+
+        }
+
+        /// <summary>
+        /// 下側へ並べる
+        /// </summary>
+        public PieceList UnderSideSort(Board board,PieceList pieceList)
+        {
+
+        }
+
+    }
+
+}
